e52a859aff93cd22df154119d2138d5887a83614
Breehze <boris.ftorek@gmail.com>
2025-07-24 19:29:47 +0200

    Storage path creation

--- src/fsops.c
+++ src/fsops.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <jansson.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
-static char path[300] = {0};
+#define PATH_BUFFER_SIZE 300
+
+static char path[PATH_BUFFER_SIZE] = {0};
 
 void FSstorageSetup(const char * target){
     const char * homeDir = getenv("HOME");
     snprintf(path,sizeof(path) / sizeof(char),"%s%s",homeDir,target);
+    FILE* targetDir = fopen(path,"w"); 
+    
+    if(!targetDir) {
+        char createPath[PATH_BUFFER_SIZE] = {0};  
+        for(size_t i = 0; i < strlen(path);i++){
+            if(path[i]== '/'){
+                mkdir(createPath,S_IRWXU);
+            }
+            createPath[i] = path[i];
+        }
+        targetDir = fopen(createPath,"w");
+        
+        if(targetDir) fprintf(targetDir,"%s","[]");
+    }
+    
+    fclose(targetDir);
 }
 
 json_t * FSopenJson(){
